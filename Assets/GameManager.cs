12509523bc75030babc1076c292908c30b9c5c2f
mbhsvr <mbhs.vr@gmail.com>
2017-06-21 16:09:45 -0400

    initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public Scrollbar rotX;
+	public Scrollbar rotY;
+	public Scrollbar rotZ;
+	public Text rotText;
+
+	public Transform cube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cube.rotation = Quaternion.Euler (360*rotX.value, 360*rotY.value, 360*rotZ.value);
+		rotText.text = "X: " + 360*rotX.value + "\nY: " +  360*rotY.value + "\nZ: " +  360*rotZ.value;
+	}
+}
